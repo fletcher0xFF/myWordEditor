e2326a54f3f8dc76c64a1615228b2ecb75b784d4
Ashley Fletcher <fletchertron@gmx.com>
2018-01-22 08:25:19 -0500

    Style changes
    Moved end brackets to conform with common JavaScript style conventions.

--- editor/scripts.js
+++ editor/scripts.js
@@ -1,11 +1,11 @@
 //stuff for plain editor
-	function getEditorText (s) {
-		return ($("#idTextArea").val ());
-		}
-	function setEditorText (s) {
-		$("#idTextArea").val (s);
-		}
-	function initEditor () {
-		var placeholder = "Obviously this is a good place to write something.";
-		$("#idEditorContainer").append ('<textarea class="myTextArea" id="idTextArea" onKeyUp="keyupTextArea ()" placeholder="' + placeholder + '"></textarea>');
-		}
+function getEditorText (s) {
+	return ($("#idTextArea").val ());
+}
+function setEditorText (s) {
+	$("#idTextArea").val (s);
+}
+function initEditor () {
+	var placeholder = "Obviously this is a good place to write something.";
+	$("#idEditorContainer").append ('<textarea class="myTextArea" id="idTextArea" onKeyUp="keyupTextArea ()" placeholder="' + placeholder + '"></textarea>');
+}
